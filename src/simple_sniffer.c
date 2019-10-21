319e27378bcb2ebefa78424fea39fed8379fb869
cass_dev <cass_dev@tc-apple.ru>
2019-10-21 18:17:58 +0500

    started implementing print_packet functions

--- src/simple_sniffer.c
+++ src/simple_sniffer.c
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <linux/if_ether.h>
 #include <string.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
 
 /*----------------------------------------------------------------------------*/
 
@@ -89,6 +91,64 @@ pcap_t* open_interface(const Settings*const sets, char* ebuf)
 
 /*----------------------------------------------------------------------------*/
 
+void print_ip4_info(const struct ip* ip_head)
+{
+}
+
+void print_ip6_info()
+{
+
+}
+
+void got_packet(u_char* args, const struct pcap_pkthdr* header,
+                const u_char* packet)
+{
+    static int count = 1;                   /* packet counter */
+    printf("\nPacket number %d:\n", count);
+    ++count;
+
+    int ether_sz = 14;
+    if (header->caplen < ether_sz) {
+        mwarning("   * Invalid Ether header length: %u bytes\n", header->caplen);
+        return;
+    }
+
+    const struct ether_header* ethernet = (const struct ether_header*)(packet);;
+
+    const int ip4_header_sz = 20;
+    const int ip4_header_sz = 40;
+    int ip_shift = 0;
+    unsigned char* ch = (unsigned char*)&ethernet->ether_type;
+    int packet_type = (ch[0] << 8) + ch[1];
+    switch (packet_type) {
+    case ETH_P_IP:
+        if (header->caplen < ether_sz + ip4_header_sz) {
+            mwarning("   * Invalid IpV4 header length: %u bytes\n",
+                     header->caplen - ether_sz);
+            return;
+        }
+        printf("   Protocol: IPV4\n");
+        break;
+    case ETH_P_IPV6:
+        if (header->caplen < ether_sz + ip4_header_sz) {
+            mwarning("   * Invalid IpV6 header length: %u bytes\n",
+                     header->caplen - ether_sz);
+            return;
+
+        }
+        printf("   Protocol: IPV6\n");
+        break;
+    default:
+        printf("   Protocol: unknown #%d\n", packet_type);
+        return;
+    }
+
+
+
+}
+
+/*----------------------------------------------------------------------------*/
+
 int main(int argc, char** argv)
 {
     Settings sets = {NULL, 0, NULL, 1};
@@ -98,21 +158,45 @@ int main(int argc, char** argv)
     pcap_t* pd = open_interface(&sets, ebuf);
     if (pd == NULL)
         merror("%s", ebuf);
-    if (setgid(getgid()) != 0 || setuid(getuid()) != 0)
-        fprintf(stderr, "Warning: setgid/setuid failed !\n");
 
-    bpf_u_int32 localnet = 0, netmask = 0;
+    /* make sure we're capturing on an Ethernet device */
+    if (pcap_datalink(pd) != DLT_EN10MB)
+        merror("%s is not an Ethernet\n", sets.device);
+
+    bpf_u_int32 localnet, netmask;
+    /* get network number and mask associated with capture device */
+    if (pcap_lookupnet(sets.device, &localnet, &netmask, ebuf) == -1) {
+        mwarning("Couldn't get netmask for device %s: %s\n", sets.device, ebuf);
+        localnet = 0;
+        netmask = 0;
+    }
+
+    printf("Device: %s\n", sets.device);
+    printf("Filter expression: %s\n", sets.filter);
+
     struct bpf_program fcode;
-    int Oflag = 1;			/* run filter code optimizer */
-    if (pcap_compile(pd, &fcode, sets.filter, Oflag, netmask) < 0)
+    int opt_flag = 1;			/* run filter code optimizer */
+    if (pcap_compile(pd, &fcode, sets.filter, opt_flag, netmask) < 0)
         merror("%s", pcap_geterr(pd));
 
     if (pcap_setfilter(pd, &fcode) < 0)
         merror("%s", pcap_geterr(pd));
 
-    int dlt = pcap_datalink(pd);
+    int status = pcap_loop(pd, 0, got_packet, NULL);
+    if (status == -2)
+        putchar('\n');
 
+    (void)fflush(stdout);
+
+    if (status == -1)
+        (void)fprintf(stderr, "%s: pcap_loop: %s\n",
+            "simple_sniffer", pcap_geterr(pd));
+
+
+    pcap_close(pd);
+    pcap_freecode(&fcode);
     cleanup_settings(&sets);
+    exit(status == -1 ? 1 : 0);
 }
 
 /*----------------------------------------------------------------------------*/
